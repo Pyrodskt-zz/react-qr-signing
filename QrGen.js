304007bdc170b38a71279c21a73c68f87c7cafb7
Pyrodskt <achille.brossier@viacesi.fr>
2022-02-20 17:36:59 +0100

    qrcode linked to url

--- QrGen.js
+++ QrGen.js
@@ -5,12 +5,17 @@ import './QrGen.css'
 export default function QrGen({ property }) {
     function QRopener(){
       if(property.classcode === "") return
-      return <QRCode value="http://google.com/" className='qr-code' size='255' renderAs='svg'/>
+      const url = 'http://192.168.1.43:5000/'
+      const f_url = url + '?classcode=' + property.classcode + '&period=' + property.period + "/" 
+      const container = <>
+        <h1>classcode : {property.classcode}</h1>
+        <h1>period : {property.period}</h1>
+        <QRCode value={f_url} className='qr-code' renderAs='svg'/>
+        </>
+      return container 
     }
   return (
     <div className='container-qr'>
-      <h1>classcode : {property.classcode}</h1>
-      <h1>period : {property.period}</h1>
       {QRopener()}
     </div>
   )
